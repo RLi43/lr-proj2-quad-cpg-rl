acfe4be7d4fe3b0e990c97e2bc68dde41a25eba9
Charlie <lichengkun0805@gmail.com>
2021-12-29 19:19:36 +0100

    experimenting

--- load_sb3.py
+++ load_sb3.py
@@ -2,7 +2,7 @@
 Author: Chengkun Li
 LastEditors: Chengkun Li
 Date: 2021-12-01 02:23:02
-LastEditTime: 2021-12-23 15:19:54
+LastEditTime: 2021-12-29 19:14:55
 Description: Modify here please
 FilePath: /lr-proj2-quad-cpg-rl/load_sb3.py
 '''
@@ -41,7 +41,7 @@ from utils.file_utils import get_latest_model, load_all_results
 LEARNING_ALG = "PPO"
 interm_dir = "./logs/intermediate_models/"
 # path to saved models, i.e. interm_dir + '111121133812'
-log_dir = interm_dir + '122321010925'
+log_dir = interm_dir + '122921155156'
 
 # initialize env configs (render at test time)
 # check ideal conditions, as well as robustness to UNSEEN noise during training
@@ -54,7 +54,7 @@ env_config['add_noise'] = False
 env_config['test_env'] = False
 env_config['competition_env'] = False
 
-plot_monitor = True
+plot_monitor = False
 
 
 # get latest model and normalization stats, and plot 
--- run_sb3.py
+++ run_sb3.py
@@ -16,7 +16,7 @@ from env.quadruped_gym_env import QuadrupedGymEnv
 
 
 LEARNING_ALG = "PPO" # "PPO" or "SAC"
-LOAD_NN = False # if you want to initialize training with a previous model 
+LOAD_NN = True # if you want to initialize training with a previous model 
 NUM_ENVS = 4    # how many pybullet environments to create for data collection
 USE_GPU = True # make sure to install all necessary drivers 
 
@@ -34,8 +34,8 @@ else:
     gpu_arg = "cpu"
 
 if LOAD_NN:
-    interm_dir = "./logs/intermediate_models/122221022218"
-    log_dir = interm_dir + '' # add path
+    interm_dir = "./logs/intermediate_models"
+    log_dir = interm_dir + '/122921155156' # add path
     stats_path = os.path.join(log_dir, "vec_normalize.pkl")
     model_name = get_latest_model(log_dir)
 
