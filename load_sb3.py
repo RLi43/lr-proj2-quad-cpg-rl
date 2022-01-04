65bdce5c89ebaa00dec2312659b6902a3929bd7c
Charlie <lichengkun0805@gmail.com>
2022-01-04 17:55:05 +0100

    fix conflicts

--- load_sb3.py
+++ load_sb3.py
@@ -2,11 +2,7 @@
 Author: Chengkun Li
 LastEditors: Chengkun Li
 Date: 2021-12-01 02:23:02
-<<<<<<< HEAD
-LastEditTime: 2022-01-04 17:25:15
-=======
-LastEditTime: 2022-01-04 09:34:55
->>>>>>> de8bbefb3976d11ef5476d6ef47766c64cb64254
+LastEditTime: 2022-01-04 17:54:50
 Description: Modify here please
 FilePath: /lr-proj2-quad-cpg-rl/load_sb3.py
 '''
@@ -45,11 +41,7 @@ from utils.file_utils import get_latest_model, load_all_results
 LEARNING_ALG = "PPO"
 interm_dir = "./logs/intermediate_models/"
 # path to saved models, i.e. interm_dir + '111121133812'
-<<<<<<< HEAD
-log_dir = interm_dir + '010422093728'
-=======
 log_dir = interm_dir + '010422002616'
->>>>>>> de8bbefb3976d11ef5476d6ef47766c64cb64254
 
 # initialize env configs (render at test time)
 # check ideal conditions, as well as robustness to UNSEEN noise during training
